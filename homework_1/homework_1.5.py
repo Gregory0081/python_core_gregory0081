39804f3a4ce8ec3155880e728eed762c4bdcc063
Gregory0081 <grigor.giv@gmail.com>
2026-09-14 21:30:27 +0300

    added homework 5

---
+++ homework_1/homework_1.5.py
@@ -0,0 +1,11 @@
+film = "Spider Man"
+person_name = " Piter Parker "
+print(person_name.strip())
+
+film2 = "Spider Man"
+person_name = " Piter Parker   "
+print(person_name.rstrip())
+
+film3 = "Spider Man"
+person_name = "   Piter Parker "
+print(person_name.lstrip())
