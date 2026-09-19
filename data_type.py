4d7387e7eb3fbd2c458da4f6ba35b2497974d673
Gregory0081 <grigor.giv@gmail.com>
2026-09-19 18:29:18 +0300

    added homework 8

--- data_type.py
+++ data_type.py
@@ -9,3 +9,11 @@ print(school)
 list_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #мой список
 second_element = list_list[1:2] #извлекаю с помощью среза второй элемент,
 print(second_element)
+
+str1 = "employ"
+str2 = "employment"
+if str1 in str2:
+    print("Строка1 входит в строку2")
+else:
+    print("Строка1 не входит в строку2")
+
