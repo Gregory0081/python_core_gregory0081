b94e47ef2b83fd5b601c680527a0bc14319227fb
Gregory0081 <grigor.giv@gmail.com>
2026-09-19 17:38:07 +0300

    added homework 6

--- data_type.py
+++ data_type.py
@@ -2,3 +2,6 @@ name = "Ivanou Ivan"
 words = name.split() #разбиваю на отдельные слова
 result = " ".join(reversed(words)) #разворачивааю слова и объединяю
 print(result)
+
+school = {"1а": 23, "1б": 22, "2а": 21, "2б": 20, "3а": 19, "3б": 20, "4а": 18, "4б": 24, "5а": 23, "5б": 17}
+print(school)
